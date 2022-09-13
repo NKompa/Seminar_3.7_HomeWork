ed4d4f0f18fedbe1480bf6218d33a0ed8d6591d7
NKompa <nkompa7@gmail.com>
2022-09-14 00:07:52 +0300

    Fill 2D array with double numbers

--- Task01_FillArrayDouble/Program.cs
+++ Task01_FillArrayDouble/Program.cs
@@ -1,9 +1,7 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. m = 3, n = 4.
-// 0,5  7       -2      -0,2
-// 1    -3,3    8       -9,9
-// 8    7,8     -7,1    9
-
+Console.WriteLine("Задайте количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
 double [,] array = new double [rows,columns];
@@ -12,7 +10,7 @@ for (int i = 0; i < rows; i++)
 {
     for (int j = 0; j < columns; j++)
     {
-        array[i,j] = Math.Round(new Random().NextDouble()+new Random ().Next(-10,10),2);
+        array[i,j] = array[i,j] = Math.Round(new Random().NextDouble()+new Random ().Next(-10,11),2);
         Console.Write(array[i,j]+"\t");
     }
     Console.WriteLine();
