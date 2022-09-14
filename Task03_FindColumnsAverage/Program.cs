3241aac59430a97fce2e5608f5067717fe423220
NKompa <nkompa7@gmail.com>
2022-09-14 14:27:38 +0300

    Find columns average in 2D array

---
+++ Task03_FindColumnsAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int [,] array = new int [5,4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(1,11);
+        Console.Write(array[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Среднее арифметическое: ");
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double average = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        average = average + array[i,j];
+    }
+    Console.Write(Math.Round(average/array.GetLength(0),2) + "\t");
+}
